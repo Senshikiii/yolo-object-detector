e278fad52869fa4ce1e4ce3de23b9cca15fc143e
Senshiki <yvessenshiki@gmail.com>
2026-06-02 12:15:08 +0530

    Dataset change: Blood cell dataset instead of the boilerplate one

--- src/main.py
+++ src/main.py
@@ -4,4 +4,12 @@ model = YOLO("yolo11n.pt")
 
 model.info()
 
-results = model.train(data="coco8.yaml", epochs=100, imgsz=640, batch=4, device='cpu')
+results = model.train(
+    data="../data/Blood Cell.v1i.yolov11/data.yaml",
+    epochs=30,
+    imgsz=640,
+    batch=7,
+    device='cpu'
+)
+
+
